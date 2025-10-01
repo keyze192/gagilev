3951b3901a2b3ab1bbe9c5a3dc7918ca5ced05c6
Elenawwi <is@sielom.local>
2025-10-01 12:41:58 +0500

    df

--- first_project/vadim/apps.py
+++ first_project/vadim/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HelloAppConfig(AppConfig):
+class VadimConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hello_app'
+    name = 'vadim'
