a041751ef156a71d5c081d3626a82a6d4feda3f2
Студент группы IS <is@sielom.local>
2025-10-24 11:07:53 +0500

    24.10

--- first_project/vadim/urls.py
+++ first_project/vadim/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', index),
 ]
